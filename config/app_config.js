dda0ba6e6d0cce7adf28178236e319c86a52947a
starkc1 <cameronstark@StarkAir.local>
2017-07-07 20:30:47 -0400

    added new page and routing

--- config/app_config.js
+++ config/app_config.js
@@ -4,6 +4,9 @@
         .when('/Dashboard', {
             templateUrl: 'views/DashView.html'
         })
+        .when('/DeviceData', {
+            templateUrl: 'views/DataView.html'
+        })
         .otherwise({
             redirectTo: '/Dashboard'
         })
