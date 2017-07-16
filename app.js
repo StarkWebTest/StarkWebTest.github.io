7f5d35a661082004c9ecba32787f1cd34dd77f6c
starkc1 <cameronstark@StarkAir.local>
2017-07-16 16:24:35 -0400

    testing

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 var app = angular.module('Testing',['ngRoute','ngMap','dateRangePicker']);
-
-Testing.controller('MainController', ['$scope', function($scope) {
-    $scope.dates = moment().range("2012-11-05", "2013-01-25")
-}]);
