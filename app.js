7f4b6ea3e178aea056773ed66fe8fd053a3c1b9d
starkc1 <cameronstark@StarkAir.local>
2017-07-16 16:08:48 -0400

    typo fix?

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = angular.module('Testing',['ngRoute','ngMap','dateRangePicker']);
 
-angular.module.controller('MainController', ['$scope', function($scope) {
+Testing.controller('MainController', ['$scope', function($scope) {
     $scope.dates = moment().range("2012-11-05", "2013-01-25")
 }]);
