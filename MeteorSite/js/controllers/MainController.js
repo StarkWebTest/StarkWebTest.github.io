baec5c5d1d8cf4f0f59211a30275a0500e7ccd74
Cameron Stark <starkc920@outlook.com>
2017-11-12 09:06:14 -0500

    Update MainController.js

--- MeteorSite/js/controllers/MainController.js
+++ MeteorSite/js/controllers/MainController.js
@@ -8,42 +8,63 @@ app.controller('MainController', ['$scope', function($scope) {
             status: 'Working',
             weight: '5 of 10lbs'
         },
-        {
-            name: 'Mt. Rainier Node 2',
-            location: '46.8523° N, 121.7603° W',
+           {
+            name: 'Mt. Olympus Node 7',
+            lat: '47.7170',
+            lon: '-123.3170',
             status: 'Working',
-            weight: '9 of 10lbs'
+            weight: '5 of 10lbs'
         },
-        {
-            name: 'Mt. Rainier Node 3',
-            location: '46.8523° N, 121.7603° W',
-            status: 'Not Working',
-            weight: '4 of 10lbs'
+            {
+            name: 'Mt. Olympus Node 8',
+            lat: '47.8330',
+            lon: '-123.3170',
+            status: 'Working',
+            weight: '5 of 10lbs'
         },
-        {
-            name: 'Mt. Rainier Node 4',
-            location: '46.8523° N, 121.7603° W',
-            status: 'Full',
-            weight: '10 of 10lbs'
+            {
+            name: 'Mt. Olympus Node 9',
+            lat: '47.8830',
+            lon: '-123.6330',
+            status: 'Working',
+            weight: '5 of 10lbs'
         },
-        {
-            name: 'Mt. Rainier Node 4',
-            location: '46.8523° N, 121.7603° W',
-            status: 'Full',
-            weight: '10 of 10lbs'
+            {
+            name: 'Mt. Olympus Node 10',
+            lat: '47.4950',
+            lon: '13.6020',
+            status: 'Working',
+            weight: '5 of 10lbs'
         },
-        {
-            name: 'Mt. Rainier Node 4',
-            location: '46.8523° N, 121.7603° W',
-            status: 'Full',
-            weight: '10 of 10lbs'
+            {
+            name: 'North Cascades Node 11',
+            lat: '48.5500',
+            lon: '-120.9500',
+            status: 'Working',
+            weight: '5 of 10lbs'
         },
-        {
-            name: 'Mt. Rainier Node 4',
-            location: '46.8523° N, 121.7603° W',
-            status: 'Full',
-            weight: '10 of 10lbs'
+            {
+            name: 'Noth Cascades Node 12',
+            lat: '48.3170',
+            lon: '-120.9830',
+            status: 'Working',
+            weight: '5 of 10lbs'
         },
+            {
+            name: 'North Cascades Node 13',
+            lat: '48.3670',
+            lon: '-121.0500',
+            status: 'Working',
+            weight: '5 of 10lbs'
+        },
+            {
+            name: 'Mt Olympus Node 13',
+            lat: '47.8170',
+            lon: '-123.6000',
+            status: 'Working',
+            weight: '5 of 10lbs'
+        },
+    
 
         
 
