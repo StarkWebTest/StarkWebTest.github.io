9cf04734df39c1412a8d320926be751f1662f887
Cameron Stark <starkc920@outlook.com>
2017-12-19 16:00:36 -0500

    site additions

--- MeteorSite/js/controllers/MainController.js
+++ MeteorSite/js/controllers/MainController.js
@@ -1,77 +1,93 @@
-app.controller('MainController', ['$scope', function($scope) {
-
+app.controller('MainController', ['$scope', function ($scope) {
     $scope.modules = [
         {
-            name: 'Mt. Rainier Node 1',
-            lat: '46.8523',
-            lon: '-121.7603',
-            status: 'Working',
-            weight: '5 of 10lbs'
+            location: 'Mt. Rainier',
+            code: 'rainier',
+            nodes: [
+                {
+                    $id: '1',
+                    location: 'Mt. Rainier',
+                    name: 'Node 1',
+                    lat: '46.8523',
+                    lon: '-121.7603',
+                    status: 'Working',
+                    weight: '6'
+                },
+                {
+                    $id: '2',
+                    location: 'Mt. Rainier',
+                    name: 'Node 2',
+                    lat: '47.7170',
+                    lon: '-123.3170',
+                    status: 'Full',
+                    weight: '10'
+                },
+                {
+                    $id: '3',
+                    location: 'Mt. Rainier',
+                    name: 'Node 3',
+                    lat: '47.7170',
+                    lon: '-123.3170',
+                    status: 'Not Working',
+                    weight: '4'
+                },
+                {
+                    $id: '4',
+                    location: 'Mt. Rainier',
+                    name: 'Node 4',
+                    lat: '47.7170',
+                    lon: '-123.3170',
+                    status: 'Working',
+                    weight: '2'
+                },
+                {
+                    $id: '5',
+                    location: 'Mt. Rainier',
+                    name: 'Node 5',
+                    lat: '47.7170',
+                    lon: '-123.3170',
+                    status: 'Full',
+                    weight: '10'
+                }
+            ]
         },
-           {
-            name: 'Mt. Olympus Node 7',
-            lat: '47.7170',
-            lon: '-123.3170',
-            status: 'Working',
-            weight: '5 of 10lbs'
+        {
+            location: 'Mt. Olympus',
+            code: 'olympus',
+            nodes: [
+                {
+                    $id: '6',
+                    location: 'Mt. Olympus',
+                    name: 'Node 1',
+                    lat: '47.8330',
+                    lon: '-123.3170',
+                    status: 'Not Working',
+                    weight: '5'
+                }
+            ]
         },
-            {
-            name: 'Mt. Olympus Node 8',
-            lat: '47.8330',
-            lon: '-123.3170',
-            status: 'Not Working',
-            weight: '5 of 10lbs'
-        },
-            {
-            name: 'Mt. Olympus Node 9',
-            lat: '47.8830',
-            lon: '-123.6330',
-            status: 'Working',
-            weight: '5 of 10lbs'
-        },
-            {
-            name: 'Mt. Olympus Node 10',
-            lat: '47.4950',
-            lon: '13.6020',
-            status: 'Full',
-            weight: '10 of 10lbs'
-        },
-            {
-            name: 'North Cascades Node 11',
-            lat: '48.5500',
-            lon: '-120.9500',
-            status: 'Working',
-            weight: '5 of 10lbs'
-        },
-            {
-            name: 'Noth Cascades Node 12',
-            lat: '48.3170',
-            lon: '-120.9830',
-            status: 'Working',
-            weight: '5 of 10lbs'
-        },
-            {
-            name: 'North Cascades Node 13',
-            lat: '48.3670',
-            lon: '-121.0500',
-            status: 'Working',
-            weight: '5 of 10lbs'
-        },
-            {
-            name: 'Mt Olympus Node 13',
-            lat: '47.8170',
-            lon: '-123.6000',
-            status: 'Working',
-            weight: '5 of 10lbs'
-        },
-    
+        {
+            location: 'Mt. Everest',
+            code: 'everest',
+            nodes: [
+                {
+                    $id: '7',
+                    location: 'Mt. Everest',
+                    name: 'Node 1',
+                    lat: '47.8330',
+                    lon: '-123.3170',
+                    status: 'Not Working',
+                    weight: '5'
+                }
+            ]
+        }
 
-        
 
     ];
-    
 
-   
-
+    $scope.select = function (item) {
+        $scope.selected = item;
+    }
+    $scope.selected = {};
 
 }]);
